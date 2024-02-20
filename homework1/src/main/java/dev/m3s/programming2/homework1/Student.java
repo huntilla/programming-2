d7711a842502b350a882a3ab2d826fee428b7cc7
huntilla <kalleasmundi@gmail.com>
2024-02-20 22:50:02 +0200

    homework1 version 2 moved tests to new class and changed checkBirthDate logic

--- homework1/src/main/java/dev/m3s/programming2/homework1/Student.java
+++ homework1/src/main/java/dev/m3s/programming2/homework1/Student.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.regex.PatternSyntaxException;
 
 public class Student {
+    private static Week1Tests week1Tests = new Week1Tests();
     private String firstName = ConstantValues.NO_NAME;
     private String  lastName = ConstantValues.NO_NAME;
     private int id;
@@ -140,6 +141,7 @@ public class Student {
         return false;
     }
 
+    // Checks if student can graduate. Needs to have thesis titles and required credits.
     private boolean canGraduate() {
         if (!getTitleOfBachelorThesis().equals(ConstantValues.NO_TITLE) &&
                 !getTitleOfMastersThesis().equals(ConstantValues.NO_TITLE)) {
@@ -157,6 +159,13 @@ public class Student {
         }
     }
 
+    // Generates a random Id between 1-100
+    private int getRandomId() {
+        Random rand = new Random();
+        int upperbound = 100;
+        return rand.nextInt(upperbound) + 1;
+    }
+
     public String setPersonId(final String personID) {
         //System.out.println(personID);
         //System.out.println(checkPersonIDNumber(personID));
@@ -169,7 +178,7 @@ public class Student {
         //System.out.println("month: " + month);
         int yearInt = Integer.parseInt(personID.substring(4,6));
         //System.out.println(String.format("year: %d", yearInt));
-
+        //Add year to yearInt according to character
         if (personID.charAt(6) == 'A') {
             yearInt += 2000;
         }
@@ -195,6 +204,7 @@ public class Student {
         return "Ok";
     }
 
+    // Checks if century character is correct
     private boolean checkValidCharacter(final String personID) {
         //System.out.println(personID);
         String birthDate = personID.substring(0, 6);
@@ -213,6 +223,7 @@ public class Student {
         return character == controlCharacters[remainder];
     }
 
+    // Checks if personID has correct format
     private boolean checkPersonIDNumber(final String personID) {
         //System.out.println(personID);
         if (personID == null || personID.length() != 11) {
@@ -225,7 +236,7 @@ public class Student {
         return Year.of(year).isLeap();
     }
 
-    // This is disgusting... Too lazy to fix
+    // Checks if given date is correct
     private boolean checkBirthdate(final String date) {
         //System.out.println(date);
         String[] splitDate;
@@ -233,6 +244,7 @@ public class Student {
         int month;
         int year;
 
+        // Splits date in to a list
         try {
             splitDate = date.split("\\.");
         } catch (PatternSyntaxException e) {
@@ -250,6 +262,7 @@ public class Student {
             //System.out.println(String.format("year: %d", year));
         } else return false;
 
+        // This is disgusting...
         if (year <= 0) {
             return false;
         }
@@ -268,30 +281,26 @@ public class Student {
         if (day == 29 && (month == 2 && checkLeapYear(year))) {
             return true;
         }
-        return day <= 31 && (month == 1 || month == 3 || month == 5 || month == 7
-                || month == 8 || month == 10 || month == 12);
+        return true;
+//        return day <= 31 && (month == 1 || month == 3 || month == 5 || month == 7
+//                || month == 8 || month == 10 || month == 12);
     }
 
-    private int getRandomId() {
-        Random rand = new Random();
-        int upperbound = 100;
-        return rand.nextInt(upperbound) + 1;
-    }
-
-    private String printStatus() {
+    // Methods to help with toString
+    private String toStringStatus() {
         if (hasGraduated()) {
             return String.format("The student has graduated in %d", getGraduationYear());
         } else return "The student has not graduated, yet";
     }
 
-    private String printStartYear() {
+    private String toStringStartYear() {
         if (hasGraduated()) {
             return String.format("studies lasted for %d years", getStudyYears());
         }
         return String.format("studies have lasted for %d years", getStudyYears());
     }
 
-    private String printBachelorCredits() {
+    private String toStringBachelorCredits() {
         if (getBachelorCredits() >= ConstantValues.BACHELOR_CREDITS) {
             return String.format("%.01f ==> All required bachelor credits completed " +
                     "(%.01f/%.01f)", getBachelorCredits(), getBachelorCredits(), ConstantValues.BACHELOR_CREDITS);
@@ -301,7 +310,7 @@ public class Student {
                 getBachelorCredits(), ConstantValues.BACHELOR_CREDITS);
     }
 
-    private String printMasterCredits() {
+    private String toStringMasterCredits() {
         if (getMasterCredits() >= ConstantValues.MASTER_CREDITS) {
             return String.format("%.01f ==> All required master's credits completed " +
                     "(%.01f/%.01f)", getMasterCredits(), getMasterCredits(), ConstantValues.MASTER_CREDITS);
@@ -311,6 +320,7 @@ public class Student {
                 getMasterCredits(), ConstantValues.MASTER_CREDITS);
     }
 
+    // Custom toString
     public String toString() {
         String string = String.format("Student id: %d\n" +
                 "FirstName: %s, LastName: %s\n" +
@@ -321,116 +331,19 @@ public class Student {
                 "TitleOfBachelorThesis: \"%s\"\n" +
                 "MasterCredits: %s\n" +
                 "TitleOfMastersThesis: \"%s\"\n",
-                getId(), getFirstName(), getLastName(), getBirthDate(), printStatus(),
-                getStartYear(), printStartYear(), printBachelorCredits(),  getTitleOfBachelorThesis(),
-                printMasterCredits(), getTitleOfMastersThesis());
+                getId(), getFirstName(), getLastName(), getBirthDate(), toStringStatus(),
+                getStartYear(), toStringStartYear(), toStringBachelorCredits(),  getTitleOfBachelorThesis(),
+                toStringMasterCredits(), getTitleOfMastersThesis());
 
         return string;
-    }
-
-    private static void test1() {
-        Student student1 = new Student();
-        Student student2 = new Student("Mouse", "Mickey");
-
-        student1.setFirstName("Donald");
-        student1.setLastName("Duck");
-        student1.setId(330);
-        student1.setBachelorCredits(55);
-        student1.setMasterCredits(14);
-        student1.setTitleOfBachelorThesis("Bachelor thesis title");
-        student1.setStartYear(2020);
-        student1.setGraduationYear(2021);
-
-        student2.setBachelorCredits(5);
-        student2.setId(4);
-        student2.setTitleOfBachelorThesis(null);
-
-        System.out.println(student1);
-        System.out.println(student2);
-    }
-
-    private static void test2() {
-        Student student1 = new Student();
-        Student student2 = new Student("Mouse", "Mickey");
-
-        student1.setFirstName("Donald");
-        student1.setLastName("Duck");
-        student1.setId(0);
-        student1.setBachelorCredits(180);
-        student1.setMasterCredits(180);
-        student1.setTitleOfMastersThesis("Masters thesis title");
-        student1.setTitleOfBachelorThesis("Bachelor thesis title");
-        student1.setStartYear(2001);
-        student1.setGraduationYear(2020);
-
-        System.out.println(student1);
-        System.out.println(student2);
-    }
-
-    private static void test3() {
-        Student student1 = new Student();
-        Student student2 = new Student("Mouse", "Mickey");
-
-        student1.setFirstName("Donald");
-        student1.setLastName("Duck");
-        student1.setId(0);
-        student1.setBachelorCredits(180);
-        student1.setMasterCredits(120);
-        student1.setTitleOfBachelorThesis("Bachelor thesis title");
-        student1.setStartYear(2021);
-        student1.setGraduationYear(2021);
-
-        student2.setFirstName(null);
-        student2.setLastName(null);
-        student2.setBachelorCredits(180);
-        student2.setMasterCredits(120);
-        student2.setTitleOfBachelorThesis("How to survive a bachelors thesis");
-        student2.setTitleOfMastersThesis("Happy ending");
-        student2.setId(101);
-
-        System.out.println(student1);
-        System.out.println(student2);
-
-        System.out.println();
-    }
-
-    private static void test1_v2() {
-        Student student1 = new Student();
-        Student student2 = new Student("Mouse", "Mickey");
-        Student student3 = new Student("Mouse", "Minnie");
-
-        student1.setFirstName("Donald");
-        student1.setLastName("Duck");
-        student1.setBachelorCredits(120);
-        student1.setMasterCredits(180);
-        student1.setTitleOfMastersThesis("Masters thesis title");
-        student1.setTitleOfBachelorThesis("Bachelor thesis title");
-        student1.setStartYear(2001);
-        student1.setGraduationYear(2020);
-
-        student2.setPersonId("220146+611K");
-        student2.setTitleOfBachelorThesis("A new exciting purpose of life");
-        student2.setBachelorCredits(65);
-        student2.setMasterCredits(22);
-
-        student3.setPersonId("111111-3334");
-        student3.setBachelorCredits(215);
-        student3.setMasterCredits(120);
-        student3.setTitleOfMastersThesis("Christmas - The most wonderful time of the year");
-        student3.setTitleOfBachelorThesis("Dreaming of a white Christmas");
-        student3.setStartYear(2018);
-        student3.setGraduationYear(2022);
-
-        System.out.println(student1);
-        System.out.println(student2);
-        System.out.println(student3);
     }
 
     public static void main(String[] args) {
         //test1();
         //test2();
         //test3();
-        test1_v2();
+        //week1Tests.test1_v2();
+
 
         //System.out.println();
     }
---
+++ homework1/src/main/java/dev/m3s/programming2/homework1/Week1Tests.java
@@ -0,0 +1,102 @@
+package dev.m3s.programming2.homework1;
+
+public class Week1Tests {
+
+    public static void test1() {
+        Student student1 = new Student();
+        Student student2 = new Student("Mouse", "Mickey");
+
+        student1.setFirstName("Donald");
+        student1.setLastName("Duck");
+        student1.setId(330);
+        student1.setBachelorCredits(55);
+        student1.setMasterCredits(14);
+        student1.setTitleOfBachelorThesis("Bachelor thesis title");
+        student1.setStartYear(2020);
+        student1.setGraduationYear(2021);
+
+        student2.setBachelorCredits(5);
+        student2.setId(4);
+        student2.setTitleOfBachelorThesis(null);
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+
+    public static void test2() {
+        Student student1 = new Student();
+        Student student2 = new Student("Mouse", "Mickey");
+
+        student1.setFirstName("Donald");
+        student1.setLastName("Duck");
+        student1.setId(0);
+        student1.setBachelorCredits(180);
+        student1.setMasterCredits(180);
+        student1.setTitleOfMastersThesis("Masters thesis title");
+        student1.setTitleOfBachelorThesis("Bachelor thesis title");
+        student1.setStartYear(2001);
+        student1.setGraduationYear(2020);
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+
+    public static void test3() {
+        Student student1 = new Student();
+        Student student2 = new Student("Mouse", "Mickey");
+
+        student1.setFirstName("Donald");
+        student1.setLastName("Duck");
+        student1.setId(0);
+        student1.setBachelorCredits(180);
+        student1.setMasterCredits(120);
+        student1.setTitleOfBachelorThesis("Bachelor thesis title");
+        student1.setStartYear(2021);
+        student1.setGraduationYear(2021);
+
+        student2.setFirstName(null);
+        student2.setLastName(null);
+        student2.setBachelorCredits(180);
+        student2.setMasterCredits(120);
+        student2.setTitleOfBachelorThesis("How to survive a bachelors thesis");
+        student2.setTitleOfMastersThesis("Happy ending");
+        student2.setId(101);
+
+        System.out.println(student1);
+        System.out.println(student2);
+
+        System.out.println();
+    }
+
+    public static void test1_v2() {
+        Student student1 = new Student();
+        Student student2 = new Student("Mouse", "Mickey");
+        Student student3 = new Student("Mouse", "Minnie");
+
+        student1.setFirstName("Donald");
+        student1.setLastName("Duck");
+        student1.setBachelorCredits(120);
+        student1.setMasterCredits(180);
+        student1.setTitleOfMastersThesis("Masters thesis title");
+        student1.setTitleOfBachelorThesis("Bachelor thesis title");
+        student1.setStartYear(2001);
+        student1.setGraduationYear(2020);
+
+        student2.setPersonId("220146+611K");
+        student2.setTitleOfBachelorThesis("A new exciting purpose of life");
+        student2.setBachelorCredits(65);
+        student2.setMasterCredits(22);
+
+        student3.setPersonId("111111-3334");
+        student3.setBachelorCredits(215);
+        student3.setMasterCredits(120);
+        student3.setTitleOfMastersThesis("Christmas - The most wonderful time of the year");
+        student3.setTitleOfBachelorThesis("Dreaming of a white Christmas");
+        student3.setStartYear(2018);
+        student3.setGraduationYear(2022);
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+    }
+}
