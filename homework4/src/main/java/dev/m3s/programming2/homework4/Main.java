287c01f094c9e6f3181bdf751e7f445b16292680
huntilla <kalleasmundi@gmail.com>
2024-05-04 20:56:43 +0300

    homework 4 kapistelijat.png

--- homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
+++ homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         try {
             wordList = new WordList("words.txt");
-            gameMenu();
+            //gameMenu();
             game = new Hangman(wordList, amountOfGuesses);
             playGame();
         } catch (FileNotFoundException e) {
