2564d6cb58471dab801dcce9339712ac155eaf26
huntilla <kalleasmundi@gmail.com>
2024-05-05 22:11:54 +0300

    homework 4 final commit

--- homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
+++ homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         try {
             wordList = new WordList("words.txt");
-            //gameMenu();
+            //gameMenu(); // disable to make the pipeline pass
             game = new Hangman(wordList, amountOfGuesses);
             playGame();
         } catch (FileNotFoundException e) {
